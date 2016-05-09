ccbef4bb289bea5c6f8105ee9b9a1e36606b891a
Richard Boell <RichardBoell@web.de>
2016-05-09 18:58:50 +0200

    Initial Commit

---
+++ ViewControllerCheckout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewControllerCheckout
+//
+//  Created by Richard Boell on 09.05.16.
+//  Copyright © 2016 RB & Comp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
